f90123df8b10ac65a40f876be0946e3652ffbc7e
xincheng213618 <xincheng213618@gmail.com>
2021-10-11 17:47:42 +0800

    优化原本的控制逻辑，移除可能出现的BUG ，添加电机运动的等待时间

--- USB/MainWindow.xaml.cs
+++ USB/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Ports;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -39,83 +40,21 @@ namespace USB
             timer = new Timer(_ => Dispatcher.BeginInvoke(new Action(() => TimeRun())), null, 0, 1000);//本来是60，不过没必要刷新这么快，就1s1次就好。
 
             this.DataContext = MotorControl;
-
-            //pageTimer = new DispatcherTimer() { IsEnabled = false, Interval = TimeSpan.FromSeconds(1)};
-            //pageTimer.Tick += new EventHandler((sender1, e1) =>
-            //{
-
-            //    if (--Countdown > 0)
-            //    {
-            //    }
-            //    else
-            //    {
-            //        pageTimer.IsEnabled = false;
-            //        Countdown = 10;
-            //        GetComBox();
-            //    }
-            //});
         }
 
         private void TimeRun()
         {
-            OpenShow.Background = MotorControl.IsOpen ? Brushes.Red : Brushes.Green;
-            if (!MotorControl.IsOpen)
+            if (!MotorControl.Port.serialPort.IsOpen)
             {
                 int Code = MotorControl.Initialized();
                 OpenShow.Background = Code == 0 ? Brushes.Red : Brushes.Green;
             }
-        }
-
-
-        int Countdown = 10;
-        public int TempIndex = 0;
-
-
-        private void Refresh_Click(object sender, RoutedEventArgs e)
-        {
-            if (pageTimer.IsEnabled)
-                pageTimer.IsEnabled = false;
-        }
-
-
-
-        public async void OpenFast()
-        {
-            await Task.Delay(1);
-            int Code = -1 ;
-
-            if (Code == 0)
-            {
-                pageTimer.IsEnabled = false;
-
-                Helper.serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceived);
-            }
             else
             {
-                Countdown = 10;
-                pageTimer.IsEnabled = true;
+                OpenShow.Background = MotorControl.Port.serialPort.IsOpen ? Brushes.Red : Brushes.Green;
             }
         }
 
-
-
-        public async void Open(string PortName)
-        {
-            await Task.Delay(1);
-            int Code = Helper.OpenPort(PortName);
-            MessageBox.Show(Code.ToString());
-
-            if (Code == 0)
-            {
-                pageTimer.IsEnabled = false;
-                Helper.serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceived);
-            }
-            else
-            {
-                Countdown = 10;
-                pageTimer.IsEnabled = true;
-            }
-        }
 
        
 
@@ -142,30 +81,40 @@ namespace USB
         }
 
 
-        private void ServiceClose()
-        {
-            Helper.Close();
-            Helper.serialPort.DataReceived -= new SerialDataReceivedEventHandler(DataReceived);
-        }
-
 
 
         private void Move_Click(object sender, RoutedEventArgs e)
         {
-            Button button = sender as Button;
-            switch (button.Tag)
+            if (!MotorControl.Port.serialPort.IsOpen)
+            {
+                MessageBox.Show("请检查位移台的是否正确连接");
+                return;
+            }
+            if (MotorControl.IsRun.IsEnabled)
+            {
+                //MessageBox.Show("正在运动中");
+                return;
+            }
+            Move(((Button)sender).Tag.ToString());
+
+
+        }
+        private  async void Move( string control)
+        {
+            switch (control)
             {
                 case "X+":
-                    MotorControl.MoveX(100);
+                    MotorControl.MoveX(int.Parse(Num.Text));
+
                     break;
                 case "X-":
-                    MotorControl.MoveX(-100);
+                    MotorControl.MoveX(-int.Parse(Num.Text));
                     break;
                 case "Y+":
-                    MotorControl.MoveY(100);
+                    MotorControl.MoveY(int.Parse(Num.Text));
                     break;
                 case "Y-":
-                    MotorControl.MoveY(-100);
+                    MotorControl.MoveY(-int.Parse(Num.Text));
                     break;
                 case "X++":
                     MotorControl.MoveX(1000);
@@ -186,20 +135,49 @@ namespace USB
                     MotorControl.ReSetMove();
                     break;
                 case "Z+":
-                    MotorControl.MoveZ(-100);
+                    MotorControl.MoveZ(int.Parse(Num.Text));
                     break;
                 case "Z-":
-                    MotorControl.MoveZ(100);
+                    MotorControl.MoveZ(-int.Parse(Num.Text));
+                    break;
+                case "Change":
+                    int Z = MotorControl.Z;
+                    int ZHlight = int.Parse(Num.Text);
+
+
+                    ZHlight = int.Parse(Num.Text);
+                    //MotorControl.MoveZ(ZHlight);
+
+                    //await Task.Delay(ZHlight / 15 + 1000);
+                    //MotorControl.MoveZ(-ZHlight);
+
+                    MotorControl.MoveY(ZHlight);
+                    await Task.Delay(ZHlight / 15 + 700);
+                    MotorControl.MoveY(ZHlight);
+
+                    break;
+                case "ChangeX":
+                    ZHlight = int.Parse(Num.Text);
+                    //MotorControl.MoveZ(ZHlight);
+                    MotorControl.MoveY(ZHlight);
+                    await Task.Delay(ZHlight / 15 + 700);
+                    MotorControl.MoveY(-ZHlight);
+
                     break;
                 default:
                     break;
             }
         }
 
+
+
+
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
 
         }
+
+       
 
         private void Len_Ini(object sender, RoutedEventArgs e)
         {
@@ -219,5 +197,13 @@ namespace USB
             int x = LenControl.Read();
             MessageBox.Show(x.ToString());
         }
+
+
+        private void PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
     }
+
 }
--- USBHelper/MotorControl.cs
+++ USBHelper/MotorControl.cs
@@ -8,16 +8,125 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace USBDLL
 {
-
-
     /// <summary>
     /// 电机控制封装部分
     /// </summary>
     public class MotorControl: INotifyPropertyChanged
     {
+        public static class Port
+        {
+            public static SerialPort serialPort = new SerialPort { };
+            public static string SetMsg = "/1aM1j16m30h15L100V16000aM2j16m30h15L100V16000aM3j16m30h15L100V16000aM4j16m30h15L100V16000R\r";
+            public static int OpenPort(string PortName)
+            {
+                try
+                {
+                    if (!serialPort.IsOpen)
+                    {
+                        serialPort = new SerialPort { PortName = PortName, BaudRate = 9600 };
+                        serialPort.Open();
+                        byte[] buffer = System.Text.Encoding.UTF8.GetBytes(SetMsg);
+                        serialPort.Write(buffer, 0, buffer.Length);
+
+                        for (int i = 0; i < 10; i++)
+                        {
+                            Thread.Sleep(16);
+                            int bytesread = serialPort.BytesToRead;
+                            if (bytesread > 0)
+                            {
+                                byte[] buff = new byte[bytesread];
+                                serialPort.Read(buff, 0, bytesread);
+                                if (buff.Length == 8 && buff[3] == 64)
+                                {
+                                    serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceived);
+
+                                    return 0;
+                                }
+                            }
+                        }
+                        serialPort.Close();
+                        return -1;
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+                catch
+                {
+                    return -2;
+                }
+            }
+            public static Dictionary<int, string> OpenCode = new Dictionary<int, string>()
+        {
+            { 0,"正常" },
+            { -1,"端口异常" },
+            { -2,"端口占用/不存在端口" },
+        };
+
+            private static void DataReceived(object sender, SerialDataReceivedEventArgs e)
+            {
+                SerialPort serialPort = sender as SerialPort;
+                Thread.Sleep(50);
+                int bytesread = serialPort.BytesToRead;
+                byte[] buff = new byte[bytesread];
+                serialPort.Read(buff, 0, bytesread);
+                //这里必须要用异步,返回原本线程
+            }
+
+            public static void Close()
+            {
+                if (serialPort.IsOpen)
+                {
+                    serialPort.Close();
+                    serialPort.Dispose();
+                }
+            }
+
+
+            public static void SendMsg(byte[] msg)
+            {
+                if (serialPort.IsOpen)
+                    serialPort.Write(msg, 0, msg.Length);
+            }
+
+            public static  void SendMsg(string Msg)
+            {
+                Msg.Replace("\n", "").Replace(" ", "").Replace("\t", "").Replace("\r", "");
+                Msg += "\r";
+                SendMsg(System.Text.Encoding.UTF8.GetBytes(Msg));
+            }
+
+        }
+
+        public int runWait =0;
+
+        public int RunWait
+        {
+            get => runWait; set
+            {
+                runWait = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        public DispatcherTimer IsRun = null;
+
+        public MotorControl()
+        {
+            IsRun = new DispatcherTimer() { IsEnabled = false, Interval = TimeSpan.FromMilliseconds(100) };
+            IsRun.Tick += new EventHandler((sender, e) =>
+            {
+                IsRun.IsEnabled = RunWait-- > 1;
+
+            });
+        }
+
 
         public event PropertyChangedEventHandler PropertyChanged;
         private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
@@ -66,28 +175,40 @@ namespace USBDLL
         }
 
 
-        /// <summary>
-        /// 是否打开
-        /// </summary>
-        public bool IsOpen = false;
-
+        //重连优化代码(保留上次的串口)
+        string RePortName = null;
 
         //初始化
         public int Initialized()
         {
-            string[] PortNames = SerialPort.GetPortNames();
+
+            string[] PortNames;
+            string[] TempPortNames = SerialPort.GetPortNames();
+            if (RePortName!=null && TempPortNames.Contains(RePortName))
+            {
+                PortNames = new string[TempPortNames.Length + 1];
+                TempPortNames.CopyTo(PortNames, 1);
+                PortNames[0] = RePortName;
+                PortNames = PortNames.Distinct().ToArray();
+            }
+            else
+            {
+                PortNames = TempPortNames;
+            }
+
             //这种写法不允许有多个串口；
             for (int i = 0; i < PortNames.Count(); i++)
             {
-                int x = Helper.OpenPort(PortNames[i]);
-                if (x == 0)
+                if (Port.OpenPort(PortNames[i]) == 0)
                 {
-                    Helper.serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceived);
-                    IsOpen = true;
+                    RePortName = PortNames[i];
+                    Port.serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceived);
                     return 0;
                 }
             }
             return -1;
+
+
         }
 
         private void DataReceived(object sender, SerialDataReceivedEventArgs e)
@@ -144,9 +265,7 @@ namespace USBDLL
         /// <param name="z">Z距离</param>
         public void Move(int x = 0,int y = 0,int z = 0)
         {
-            IsOpen = Helper.serialPort.IsOpen;
-
-            if (IsOpen)
+            if (Port.serialPort.IsOpen)
             {
                 //发送0位移指令会一直运动，所以切换位1指令
 
@@ -154,14 +273,28 @@ namespace USBDLL
                 string y1 = y == 0 ? "" : y.ToString();//左为正
                 string z1 = z == 0 ? "" : (-z).ToString();//上为正
 
-
-                string Msg = $"/1P{x1},{y1},{z1},R";
-                //D指令
-                Helper.SendMsg(Msg);
-
                 X += x;
                 Y += y;
                 Z += z;
+
+                x = Math.Abs(x);
+                y = Math.Abs(y);
+                z = Math.Abs(z);
+
+                int max = x > y ? (x > z ? x : z) : (y > z ? y : z);
+
+                RunWait = (int)((0.052 * max + 250) / 100);
+                IsRun.IsEnabled = true;
+
+                string Msg = $"/1P{x1},{y1},{z1},R";
+                //D指令
+                Port.SendMsg(Msg);
+                
+                
+                
+
+
+
             }
 
         }
